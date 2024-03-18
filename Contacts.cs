9a6a85b4b57f72561787b002c9c223f5e20b3257
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-18 12:48:12 +0530

    add: Contacts.cs which is poco class

---
+++ Contacts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Contacts
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Email { get; set; }
+        private string PhoneNumber { get; set; }
+        private string Address { get; set; }
+        private string City { get; set; }
+        private string State { get; set; }
+        private string ZipCode { get; set; }
+        
+        public Contacts(string firstname, string lastname, string email,string phonenumber, string address, string city,string state, string zipcode ) 
+        
+        {
+            FirstName = firstname;  
+            LastName = lastname;    
+            Email = email;
+            PhoneNumber = phonenumber;
+            Address = address;
+            City = city;
+            State = state;
+            ZipCode = zipcode;
+
+        }
+    }
+}
